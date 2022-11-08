10ab676c4120a4d85cbff5df1848142fd153f3cf
unknown <elhossenenour@gmail.com>
2022-11-09 00:17:00 +0200

    NVIC

---
+++ Task 3/common/platform_types.h
@@ -0,0 +1,16 @@
+#ifndef Platform_Types_H_
+#define	Platform_Types_H_
+
+typedef unsigned char           uint8  ;
+typedef unsigned short int      uint16 ;
+typedef unsigned long           uint32 ;
+
+typedef signed char           sint8  ;
+typedef signed short int      sint16 ;
+typedef signed long           sint32 ;
+
+typedef float                 fint32;
+typedef double                fint64;
+
+
+#endif /*Platform_Types_H_*/
